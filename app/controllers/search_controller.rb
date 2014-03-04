672e94af37b3185e204acb078420584546bc5e78
Dan Andreasson <danvarnamnet@gmail.com>
2014-03-04 10:42:49 +0100

    Fixed layout. Now floating accordingly

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,6 @@
 class SearchController < ApplicationController
   def newsearch
     @rooms = Room.all.limit(3)
-    @objects = current_user.rooms
-    @messages = current_user.messages
     render 'search/results'
   end
 
